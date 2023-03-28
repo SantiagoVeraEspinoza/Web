6508132ceb114878d7c93ee3816bdc14157f65b6
TitanPro47 <santiveraespinoza@gmail.com>
2023-03-28 09:59:37 -0600

    Servidor

--- Tec/COSTOD/Servicios Web/Web-APIs/client/src/App.js
+++ Tec/COSTOD/Servicios Web/Web-APIs/client/src/App.js
@@ -1,7 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +25,9 @@ function App() {
         >
           Learn React
         </a>
+        <p>
+          {!data ? "Loading..." : data}
+        </p>
       </header>
     </div>
   );
